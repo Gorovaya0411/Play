69f378b7d4e8ba8c38e37f3cedf9474cc6bb78e9
dev <gorovaya-k@mail.ru>
2021-03-11 17:29:48 +0300

    +

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.myapplication
 
-import android.animation.Animator
 import android.animation.ValueAnimator
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.view.View
 import android.view.animation.LinearInterpolator
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,69 +16,58 @@ class MainActivity : AppCompatActivity() {
     val DEFAULT_ANIMATION_DURATION = 5000L
     var mScreenHeight: Float = 0.toFloat()
     var mScreenWidth: Float = 0.toFloat()
-    var in2 = 0
-     private var  value :Float= 0f
+    var score = 0
+    private var value: Float = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        imageView.visibility = ImageView.INVISIBLE
-        button2.setOnClickListener { onStartAnimation() }
-        imageView.setOnClickListener{
-            textView.text = "$in2"
-            in2++
+        buttonPanel.visibility = CardView.INVISIBLE
+        viewShadow.visibility = View.INVISIBLE
+        imageViewPresent.visibility = ImageView.INVISIBLE
+        buttonStart.setOnClickListener {
+            imageViewPresent.visibility = ImageView.VISIBLE
+            cardViewButton.visibility = CardView.INVISIBLE
+            buttonPanel.visibility = CardView.VISIBLE
+            viewShadow.visibility = View.VISIBLE
+            onStartAnimation() }
+        imageViewPresent.setOnClickListener {
+            score++
+            textViewGlasses.text = "$score"
+            onStartAnimation()
+        }
+        imageButtonReturn.setOnClickListener {
+            score = 0
+            textViewGlasses.text = "$score"
             onStartAnimation()
         }
 
     }
 
-    private fun onStartAnimation(){
-        ff.visibility = CardView.INVISIBLE
-        imageView.visibility = ImageView.VISIBLE
+    private fun onStartAnimation() {
+        cardViewButton.visibility = CardView.INVISIBLE
+        imageViewPresent.visibility = ImageView.VISIBLE
         val r = Random()
         val i1: Float = r.nextFloat() * mScreenWidth
         val i2: Float = r.nextFloat() * 3 - 2
 
         val valueAnimator = ValueAnimator.ofFloat(-mScreenHeight, mScreenHeight)
         valueAnimator.addUpdateListener { animation ->
-             value = animation.animatedValue as Float
-//            imageView.scaleX = i2
-//            imageView.scaleY = imageView.scaleX
-            imageView.translationY = value
-            imageView.translationX = i1
+            value = animation.animatedValue as Float
+            imageViewPresent.translationY = value
+            imageViewPresent.translationX = i1
         }
 
-       }
 
-        valueAnimator.addListener(object:Animator.AnimatorListener() {
-            override fun onAnimationStart(animation:Animator) {
-
-            }
-            override fun onAnimationEnd(animation:Animator) {
-
-                Toast.makeText(this@WithListenerAnimationActivity, "Doge is on the moon", Toast.LENGTH_SHORT)
-                    .show()
-                finish()
-            }
-            override fun onAnimationCancel(animation:Animator) {
-            }
-            override fun onAnimationRepeat(animation:Animator) {
-            }
-        })
         valueAnimator.interpolator = LinearInterpolator()
         valueAnimator.duration = DEFAULT_ANIMATION_DURATION
 
         valueAnimator.start()
     }
 
-
-
-
-
-
     override fun onResume() {
         super.onResume()
         val displaymetrics = DisplayMetrics()
-        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics)
+        windowManager.getDefaultDisplay().getMetrics(displaymetrics)
         mScreenHeight = displaymetrics.heightPixels.toFloat()
         mScreenWidth = displaymetrics.widthPixels.toFloat()
     }
